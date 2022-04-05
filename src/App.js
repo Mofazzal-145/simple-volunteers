e4f1a99875b70c6c0727a94a982205a922dc2993
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-06 01:49:14 +0600

    connection between use(state & effect)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home'
 import Volunteers from './components/Volunteers/Volunteers'
 import './App.css';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Main></Main>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='volunteers' element={<Volunteers></Volunteers>}></Route>
       </Routes>
     </div>
