975fe6fc2149c3d30ca5a1eb29ce70e08cd377e4
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-06 11:22:23 +0600

    work with dependency

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,18 +5,33 @@ import './Main.css'
 
 const Main = () => {
     
-    const [volunteers, setVolunteers] = useVolunteers([]);
+    // const [volunteers, setVolunteers] = useVolunteers([]);
+    const [searchText, setSearchText] = useState('');
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect( ()=>{
+        console.log('mod kaba naki')
+       fetch('data.json')
+       .then(res => res.json())
+       .then(data =>{
+           const match = data.filter(d => d.title.includes(searchText));
+           setSearchResult(match);
+       })
+    },[searchText])
+
     const handleSearchChange = event =>{
+        setSearchText(event.target.value);
+    }
+
+    /* const handleSearchChange = event =>{
         const searchText = event.target.value;
         const match = volunteers.filter(v => v.title.includes(searchText));
         setSearchResult(match);
-    }
+    } */
 
     return (
         <div>
-            <h3>Volunteers Activities:{volunteers.length}</h3>
+            <h3>Volunteers Activities:{searchResult.length}</h3>
                 <div style={{'margin' : '20px'}}>
                     <input onChange={handleSearchChange} type="text" placeholder='search' />
                 </div>
