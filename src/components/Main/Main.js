bbd20250f1c1b060cb2604426f8768828b15b476
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-06 10:58:24 +0600

    implement search function

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,13 +6,24 @@ import './Main.css'
 const Main = () => {
     
     const [volunteers, setVolunteers] = useVolunteers([]);
-    
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleSearchChange = event =>{
+        const searchText = event.target.value;
+        const match = volunteers.filter(v => v.title.includes(searchText));
+        setSearchResult(match);
+    }
+
     return (
         <div>
             <h3>Volunteers Activities:{volunteers.length}</h3>
+                <div style={{'margin' : '20px'}}>
+                    <input onChange={handleSearchChange} type="text" placeholder='search' />
+                </div>
                 <div className="activity-container">
                   {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
+                    // volunteers.map(activity => <Activity
                     key={activity._id}
                     activity={activity}
                     ></Activity>)
